066afedc77905e9db6e8137cd93dd61c99feba4b
Irina Borovtsova <iborovtsova@satelliz.com>
2016-05-18 03:16:52 +0300

    Further setup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,25 +1,69 @@
 'use strict';
 
 angular
-  .module('mapDemoApp', ['ngMaterial'])
+  .module('mapDemoApp', ['ngMaterial', 'leaflet-directive'])
   .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
-      .primaryPalette('blue', {
-        'default': '800'
+      .primaryPalette('light-blue', {
+        'default': '900'
+      })
+      .backgroundPalette('grey', {
+        'default': '50'
       });
   })
-  .directive('leafletMap', function() {
-    return {
-      restrict: 'E',
-      replace: true,
-      template: '<div id=\'map\' style="position:absolute; width:100%; height:100%;"></div>',
-      link: function(scope, element, attrs) {
-
-        var map = L.map(attrs.id).setView([42.3610, -71.0587], 15); //.setView([49.8433784,24.0276888], 12);
-        L.tileLayer('http://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token={token}', {
-          //  maxZoom: 12,
+  .controller('DemoAppCtrl', ['$scope', function($scope) {
+    angular.extend($scope, {
+      london: {
+        lat: 42.3610,
+        lng: -71.0587,
+        zoom: 15
+      },
+      // layers: {
+      //   baselayers: {
+      //     googleTerrain: {
+      //       name: 'Google Terrain',
+      //       layerType: 'TERRAIN',
+      //       type: 'google'
+      //     },
+      //     googleHybrid: {
+      //       name: 'Google Hybrid',
+      //       layerType: 'HYBRID',
+      //       type: 'google'
+      //     },
+      //     googleRoadmap: {
+      //       name: 'Google Streets',
+      //       layerType: 'ROADMAP',
+      //       type: 'google'
+      //     }
+      //   }
+      // },
+      defaults: {
+        tileLayer: 'http://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token={token}',
+        tileLayerOptions: {
+          opacity: 0.9,
+          detectRetina: true,
+          reuseTiles: true,
           token: 'pk.eyJ1IjoiaWJvcm92dHNvdmEiLCJhIjoiY2lvYnd5ejR6MDA0c3czbTVwOWx5Zmc1MCJ9.v1O7fqyBZOnRMsHbEZgwCQ'
-        }).addTo(map);
-      }
-    }
-  });
+        },
+        scrollWheelZoom: false
+      },
+      events: {}
+    });
+
+    $scope.markers = [];
+    $scope.$on('leafletDirectiveMap.click', function(event, args) {
+      var leafEvent = args.leafletEvent;
+      $scope.markers.push({
+        lat: leafEvent.latlng.lat,
+        lng: leafEvent.latlng.lng,
+        message: 'My Added Marker',
+        draggable: true
+      });
+    });
+
+    $scope.$watch(function() {
+      return angular.element(document.querySelector('.map-container'))[0].offsetHeight;
+    }, function(newValue) {
+      $scope.mapHeight = newValue;
+    });
+  }]);
